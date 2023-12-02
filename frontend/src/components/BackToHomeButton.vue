a2e99fe9da18870dd4ee225731ed7b1db4ac1c0c
VitorJuvino <juvinovitor@gmail.com>
2023-12-02 15:32:34 -0300

    new

---
+++ frontend/src/components/BackToHomeButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <router-link to="/products" class="btn btn-secondary">Lista de Produtos</router-link>
+</template>
+
+<script>
+export default {
+  name: 'BackToHomeButton'
+};
+</script>
+
+<style scoped>
+/* Estilos específicos do componente podem ser adicionados aqui */
+</style>
---
+++ frontend/src/components/LogoutButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <button @click="logout" class="btn btn-secondary">Sair</button>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout() {
+      // Limpa o token do localStorage
+      localStorage.removeItem('token');
+      
+      this.$router.push('/');
+    }
+  }
+};
+</script>
